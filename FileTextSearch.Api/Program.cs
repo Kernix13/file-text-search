10c08d0ffcbf986e537039c283d77ee1fcb3f1bf
Jim <jimkernicky@gmail.com>
2026-07-11 19:55:37 -0400

    add scalar to Api project

--- FileTextSearch.Api/Program.cs
+++ FileTextSearch.Api/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +15,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
