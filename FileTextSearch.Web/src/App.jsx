22758d49003326d485afa91dc8067be1fbf20bdc
Jim <jimkernicky@gmail.com>
2026-07-24 17:32:22 -0400

    add fetch and useState, output API results to DOM

--- FileTextSearch.Web/src/App.jsx
+++ FileTextSearch.Web/src/App.jsx
@@ -4,6 +4,7 @@ import SearchForm from "./components/SearchForm";
 const API_URL = 'http://localhost:5042/api/search';
 
 const App = () => {
+  // const [searchPhrase, setSearchPhrase] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -27,6 +28,33 @@ const App = () => {
 
   return ( 
     <div>
+      <h1>System File Search Using C#</h1>
+      {/* { loading ? <p>Loading...</p> : null } */}
+      { loading && <p>Loading...</p> }
+
+      { error && <p className="error">{ error }</p> }
+
+      { !loading && !error && (
+        <>
+          {/* <h2>Search results for "{searchPhrase}"</h2> */}
+          <main className="grid">
+            { searchResults.map(result => (
+              <div key={result.id} className="result-card">
+                {/* just a list for now: */}
+                <button>Delete</button>
+                <ul>
+                  <li>{result.category}</li>
+                  {/* <li>{result.fileName}</li> */}
+                  <li><a className="result-link" target="_blank" href="https://www.google.com/">{result.fileName}</a></li>
+                  <li>{result.priority}
+                    <button>Edit</button>
+                  </li>
+                </ul>
+              </div>
+            )) }
+          </main>
+        </>
+      ) }
       <SearchForm />
     </div>
    );
